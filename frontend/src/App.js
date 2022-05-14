b9ca7e8ed00e66d995c93cceb271daba8eef6e95
Abdullah Adeel <easyautosol@gmail.com>
2022-05-14 18:37:32 +0500

    ✅ setup `react-router-dom`

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <>
+      <Router>
+        <Routes path="/">
+          {/* Auth */}
+          <Route path="/login" element={<h1>Login</h1>} />
+          <Route path="/register" element={<h1>Register</h1>} />
+          {/* Application */}
+          <Route index element={<h1>Home</h1>} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
